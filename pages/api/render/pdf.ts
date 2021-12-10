7ef56b70d7d32c1c43cf05d44050b3ecb38bd8e3
Edouard Cockenpot <6573602+edcock@users.noreply.github.com>
2021-12-10 13:30:45 +0100

    headless chrome for production

--- pages/api/render/pdf.ts
+++ pages/api/render/pdf.ts
@@ -1,21 +1,28 @@
 import { stat } from "fs";
 import type { NextApiRequest, NextApiResponse } from "next";
 import puppeteer from "puppeteer";
+import chrome from "chrome-aws-lambda";
 
 type Data = {
   pdf: string;
 };
 async function printPdf(req: NextApiRequest, res: NextApiResponse) {
-  const puppeteer = require("puppeteer");
-
-  const browser = await puppeteer.launch();
+  const options = {
+    args: chrome.args,
+    executablePath: await chrome.executablePath,
+    headless: true,
+  };
+  const browser = await puppeteer.launch(options);
   const page = await browser.newPage();
+  console.log(typeof req.query.url);
 
-  await page.goto(req.query.url, {
+  const url: string = `${req.query.url}`;
+
+  await page.goto(url, {
     waitUntil: "networkidle2",
   });
   const pdf = await page.pdf({
-    format: "A4",
+    format: "a4",
     margin: { top: "1cm", bottom: "1cm", left: "1cm", right: "1cm" },
   });
   await page.close();
